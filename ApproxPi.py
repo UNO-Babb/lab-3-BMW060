be0a0ccce06d52630e239dfc6c9c8042676d0858
BMW060 <brennan.m.wood@gmail.com>
2025-02-06 22:05:43 +0000

    ApproxPi complete

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
 #Name:Brennan Wood 
 #Date: 2/6/25
-#Assignment: Lab 3
+#Assignment: Lab 3 (ApproxPi)
 import math
 import time
 
@@ -13,14 +13,26 @@ def main():
 
   #ask user for decimal percision (up to 10)
 
+  precision = input("Please enter the number of digits you would like to calculate pi to: ")
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
 
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxPi, int(precision)) != round(realPi, int(precision)):
+    print(approxPi)
+    approxPi = approxPi + (sign * 4/denom)
+    sign = sign * -1
+    denom = denom + 2
+    
+        
+
   end = time.time()
 
   elapsedTime = end - start
-  print(elapsedTime)
+  print("The time it took to reach this conclusion was: " + str(round(elapsedTime, 5)) + " seconds")
 
 if __name__ == '__main__':
   main()
